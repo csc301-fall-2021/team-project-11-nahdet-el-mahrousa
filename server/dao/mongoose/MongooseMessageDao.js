616ea6eec83e02b78f4782aeb7c25794e6faadb9
Yuxuan Liu <yuxuanleon.liu@mail.utoronto.ca>
2021-10-22 19:09:21 -0400

    Updated MongooseDaos and unit tests - Added MongooseMessageDao and MongooseReplyDao methods. - Added some unit tests for mongoose Daos. - Added disconnect to mongoose handler. - Fixed Reply model ObejctId type.

--- server/dao/mongoose/MongooseMessageDao.js
+++ server/dao/mongoose/MongooseMessageDao.js
@@ -5,40 +5,83 @@ class MongooseMessageDao {
     constructor(db) {
         this.db = db
         this.mongoose = db.mongoose;
+        this.Message = db.models.Message
     }
 
+    /**
+     * Create a message.
+     * @param {String} content Content of message to show the user.
+     * @param {String} label Label of message to show the admin.
+     * @returns Created Message.
+     */
     async create({ content, label }) {
-        this.db.models.Message.create({ content, label }, (err, newMessage) => {
-            if (err) {
-                logger.log(err)
-                return null
-            } else {
-                logger.log(`MONGOOSE CREATED Message ${newMessage._id}`)
-                return newMessage
-            }
-        })
+        try {
+            const newMessage = new this.Message({ content, label })
+            const createdMessage = await newMessage.save()
+            logger.log(`MONGOOSE CREATED Message ${createdMessage._id}`)
+            return newMessage
+        } catch (err) {
+            logger.log(err)
+            return undefined
+        }
     }
 
+    /**
+     * Get a message by id.
+     * @param {Integer} mid id of Message.
+     * @returns Message; If message not found, return null.
+     */
     async get(mid) {
-        const message = await this.models.Message.findById(mid).exec()
+        const message = await this.Message.findById(mid).exec()
         logger.log(`MONGOOSE GET Message ${message}`)
         return message
     }
 
+    /**
+     * Get all messages.
+     * @returns Array of Messages
+     */
     async getAll() {
-        return null
+        return await this.Message.find().exec()
     }
 
+    /**
+     * Search for messages that satisfy the filter.
+     * @param {Object} filter Field conditions that the messages must satisfy.
+     * @returns Array of Messages.
+     */
     async search(filter = null) {
-        return null
+        if (filter) {
+            // TODO: apply filter check.
+            const messages = await this.Message.find(filter).exec()
+            return messages
+        } else {
+            const messages = await this.Message.find().exec()
+            return messages
+        }
     }
 
+    /**
+     * Delete a message.
+     * @param {Integer} mid id of message.
+     * @returns Deleted Message； If message not found, return null.
+     */
     async delete(mid) {
-        return null
+        const deletedMessage = await this.Message.findByIdAndRemove(mid)
+        logger.log(`MONGOOSE DELETE Message ${deletedMessage._id}`)
+        return deletedMessage
     }
 
+    /**
+     * Update a message.
+     * @param {Integer} mid id of message.
+     * @param {Object} data Data to be updated.
+     * @returns Updated message; If message not found, return null.
+     */
     async update(mid, data) {
-        return null
+        const updatedMessage = await this.Message.findByIdAndUpdate(mid, data, { new: true })
+        logger.log(`MONGOOSE UPDATED Message to ${updatedMessage}`)
+        return updatedMessage
     }
 
 }
--- server/dao/mongoose/MongooseReplayDao.js
+++ server/dao/mongoose/MongooseReplayDao.js
@@ -1,21 +1,89 @@
 const mongoose = require('mongoose')
 
 class MongooseReplyDao {
-    constructor(db){
+    constructor(db) {
         this.db = db;
+        this.mongoose = db.mongoose;
+        this.Reply = db.models.Reply
     }
 
-    // async create({content, label}){
+    /**
+     * Create a reply.
+     * @param {String} content Content of reply to show the user.
+     * @param {String} label Label of reply to show the admin.
+     * @param {mongoose.Schema.Types.ObjectId} fromMessage The message that this replies/option belongs to.
+     * @param {mongoose.Schema.Types.ObjectId} toMessage The message that this replies redirects to.
+     * @returns Created Reply.
+     */
+    async create({ content, label, fromMessage, toMessage }) {
+        try {
+            const newReply = new this.Reply({ content, label, fromMessage, toMessage })
+            const createdReply = await newReply.save()
+            logger.log(`MONGOOSE CREATED Reply ${createdReply._id}`)
+            return newReply
+        } catch (err) {
+            logger.log(err)
+            return undefined
+        }
+    }
 
-    // }
+    /**
+     * Get a reply by id.
+     * @param {Integer} rid id of Reply.
+     * @returns Reply; undefined if not found.
+     */
+    async get(rid) {
+        const reply = await this.Reply.findById(rid).exec()
+        logger.log(`MONGOOSE GET Reply ${reply}`)
+        return reply
+    }
 
-    // getAll(page=-1, filter=null) {
-    //     if (page !== -1){
-    //         return this.mongoose.retrieve('Reply', page=page, filter=filter)
-    //     } else {
-    //         return this.mongoose.retrieve('Reply', query=null)
-    //     }
-    // }
+    /**
+     * Get all replies.
+     * @returns Array of Replies
+     */
+    async getAll() {
+        return await this.Reply.find().exec()
+    }
+
+    /**
+     * Search for replies that satisfy the filter.
+     * @param {Object} filter Field conditions that the replies must satisfy.
+     * @returns Array of Replies.
+     */
+    async search(filter = null) {
+        if (filter) {
+            // TODO: apply filter check.
+            const replies = await this.Reply.find(filter).exec()
+            return replies
+        } else {
+            const replies = await this.Reply.find().exec()
+            return replies
+        }
+    }
+
+    /**
+     * Delete a reply.
+     * @param {Integer} rid id of reply.
+     * @returns Deleted Reply.
+     */
+    async delete(rid) {
+        const deletedReply = await this.Reply.findByIdAndRemove(rid)
+        logger.log(`MONGOOSE DELETE Reply ${deletedReply._id}`)
+        return deletedReply
+    }
+
+    /**
+     * Update a reply.
+     * @param {Integer} rid id of reply.
+     * @param {Object} data Data to be updated.
+     * @returns Updated reply.
+     */
+    async update(rid, data) {
+        const updatedReply = await this.Reply.findByIdAndUpdate(rid, data, { new: true })
+        logger.log(`MONGOOSE UPDATED Reply to ${updatedReply}`)
+        return updatedReply
+    }
 
 }
 
