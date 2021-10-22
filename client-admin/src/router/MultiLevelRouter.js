c25d0e0d291cca4a82e24efb41bd9a59f9b6a01c
Yuxuan Liu <yuxuanleon.liu@mail.utoronto.ca>
2021-10-22 01:10:02 -0400

    Initialized Admin Dashboard frontend

---
+++ client-admin/src/router/MultiLevelRouter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom"; // <-- New code
+
+/**
+ * Render a route with potential sub routes
+ * https://reacttraining.com/react-router/web/example/route-config
+ */
+function RouteWithSubRoutes(route) {
+    return (
+        <Route
+            path={route.path}
+            exact={route.exact}
+            render={props => <route.component {...props} routes={route.routes} />}
+        />
+    );
+}
+
+export default function MultiLevelRouter({ routes }) {
+    return (
+        <Switch>
+            {routes.map((route, i) => {
+                return <RouteWithSubRoutes key={route.key} {...route} />;
+            })}
+            <Route component={() => <h1>Not Found!</h1>} />
+        </Switch>
+    );
+}
