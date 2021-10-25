ec88669e73810a8f10d7474e4121dea8f92694b2
xiaoyaw2 <grace.xiao@mail.utoronto.ca>
2021-10-25 18:19:59 -0400

    app setup

--- client-app/src/pages/Home/index.js
+++ client-app/src/pages/Home/index.js
@@ -7,7 +7,7 @@ class HomePage extends React.Component {
         return (
             <div className="page home-page">
                 <div className="main-view home-main">
-                    <h1>HOME PAGE</h1>
+                    <h1>HELLO WORLD</h1>
 
                     <Menu />
                 </div>
