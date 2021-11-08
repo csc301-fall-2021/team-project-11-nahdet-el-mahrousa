9e628cf60c5591c390286a183260d7a10378c502
themarkyone <markyin2001@gmail.com>
2021-11-08 18:31:36 -0500

    update menu into bot page

--- client-admin/src/pages/Database/Bot/index.js
+++ client-admin/src/pages/Database/Bot/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Messages from "../../../components/Bot/BotMessages/messages";
 import { getMessageFromBackend } from "../../../actions/Bot/index";
 import { AddNewMessageButton } from "../../../components/Bot/NewMessageModal/index";
+import Sider from "components/Menu";
 
 class BotPage extends React.Component {
   render() {
     getMessageFromBackend();
     return (
       <div>
+        <Sider />
         <h1>Bot PAGE</h1>
         <AddNewMessageButton></AddNewMessageButton>
         <div className="container">
