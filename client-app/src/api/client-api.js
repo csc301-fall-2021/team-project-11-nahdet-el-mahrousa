39cb46fefdb8b356b0fcc32c8bc4df82f9c0babb
Yuxuan Liu <yuxuanleon.liu@mail.utoronto.ca>
2021-11-07 23:10:13 -0500

    Added error handle for not found response

--- client-app/src/api/client-api.js
+++ client-app/src/api/client-api.js
@@ -5,6 +5,9 @@ export async function getFirstMessage() {
       const response = await httpGet("/bot/message");
       if (response.statusCode === 200) {
          console.log(response);
+         if (!response.entity) {
+            throw new Error("No Entity")
+         }
          return response.entity;
       } else {
          throw new Error(response.msg);
@@ -20,6 +23,9 @@ export async function getNextMessage(data) {
       const response = await httpPost("/bot/message", data);
       if (response.statusCode === 200) {
          console.log(response);
+         if (!response.entity) {
+            throw new Error("No Entity")
+         }
          return response.entity;
       } else {
          throw new Error(response.msg);
