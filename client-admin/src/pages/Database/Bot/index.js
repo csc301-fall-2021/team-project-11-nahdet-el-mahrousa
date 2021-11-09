a2074b1e6657bbc60fcdc38c2880c297c78af3da
Serena Yu <serenayuxinyao@gmail.com>
2021-11-08 21:56:36 -0500

    changed layout

--- client-admin/src/pages/Database/Bot/index.js
+++ client-admin/src/pages/Database/Bot/index.js
@@ -3,19 +3,24 @@ import Messages from "../../../components/Bot/BotMessages/messages";
 import { getMessageFromBackend } from "../../../actions/Bot/index";
 import { AddNewMessageButton } from "../../../components/Bot/NewMessageModal/index";
 import Sider from "components/Menu";
+import { Row, Col } from 'antd';
 
 class BotPage extends React.Component {
   render() {
     getMessageFromBackend();
     return (
-      <div>
-        <Sider />
-        <h1>Bot PAGE</h1>
-        <AddNewMessageButton></AddNewMessageButton>
-        <div className="container">
-          <Messages></Messages>
-        </div>
-      </div>
+      <Row style={{ minHeight: '100vh' }}>
+        <Col>
+          <Sider></Sider>
+        </Col>
+        <Col className="site-layout" style={ {marginLeft: 20, width: 1100}}>
+          <h1>Bot PAGE</h1>
+          <AddNewMessageButton></AddNewMessageButton>
+          <div className="container">
+            <Messages></Messages>
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
