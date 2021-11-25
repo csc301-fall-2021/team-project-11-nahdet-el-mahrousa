66a378ee59c1d3997e8aacc7d4610d33592ec6d9
Serena Yu <serenayuxinyao@gmail.com>
2021-11-25 17:50:06 -0500

    changed and fixed new option select

--- client-admin/src/components/Bot/BotModal/index.js
+++ client-admin/src/components/Bot/BotModal/index.js
@@ -12,41 +12,41 @@ import EditMessageDrawer from "../EditMessageDrawer";
 
 class DeleteButton extends React.Component {
   state = {
-      visible: false,
-      confirmLoading: false
-  }
+    visible: false,
+    confirmLoading: false,
+  };
 
   handleOk = async () => {
-      this.setState({ confirmLoading: true })
+    this.setState({ confirmLoading: true });
 
-      const msgToDelete = this.props.target
-      console.log('deleting ', msgToDelete)
-      // TODO: to async request
-      try {
-          await deleteMessageToBackend(msgToDelete)
-          this.setState({ visible: false })
-          this.setState({ confirmLoading: false })
-          message.success(`Deleted message`)
-      } catch (error) {
-          this.setState({ confirmLoading: false })
-          message.error(String(error))
-      }
+    const msgToDelete = this.props.target;
+    console.log("deleting ", msgToDelete);
+    // TODO: to async request
+    try {
+      await deleteMessageToBackend(msgToDelete);
+      this.setState({ visible: false });
+      this.setState({ confirmLoading: false });
+      message.success(`Deleted message`);
+    } catch (error) {
+      this.setState({ confirmLoading: false });
+      message.error(String(error));
+    }
   };
 
   render() {
-      return (
-          <Popconfirm
-              title="Confirm?"
-              visible={this.state.visible}
-              onConfirm={this.handleOk}
-              okButtonProps={{ loading: this.state.confirmLoading }}
-              onCancel={() => this.setState({ visible: false })}
-          >
-              <Button type="primary" onClick={() => this.setState({ visible: true })}>
-                  Delete
-              </Button>
-          </Popconfirm>
-      )
+    return (
+      <Popconfirm
+        title="Confirm?"
+        visible={this.state.visible}
+        onConfirm={this.handleOk}
+        okButtonProps={{ loading: this.state.confirmLoading }}
+        onCancel={() => this.setState({ visible: false })}
+      >
+        <Button type="primary" onClick={() => this.setState({ visible: true })}>
+          Delete
+        </Button>
+      </Popconfirm>
+    );
   }
 }
 
@@ -56,40 +56,33 @@ export function MessageOptionMenu(props) {
   const [visible, setVisible] = useState(false);
   const [visibleEdit, setVisibleEdit] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const [modalTextContent, setModalTextContent] = useState('');
-  const [modalTextLabel, setModalTextLabel] = useState('');
-  const [modelToMessageId, setModelToMessageId] = useState('');
+  const [modalTextContent, setModalTextContent] = useState("");
+  const [modalTextLabel, setModalTextLabel] = useState("");
+  const [modelToMessageId, setModelToMessageId] = useState("");
   const [displayReplyWarning, setDisplayReplyWarning] = useState(false);
 
   const updateState = () => {
-    setVisible(false)
-    setVisibleEdit(false)
-    setConfirmLoading(false)
-    setModalTextContent('')
-    setModelToMessageId('')
-    setModelToMessageId('')
-    setDisplayReplyWarning(false)
-  }
+    setVisible(false);
+    setVisibleEdit(false);
+    setConfirmLoading(false);
+    setModalTextContent("");
+    setModelToMessageId("");
+    setModelToMessageId("");
+    setDisplayReplyWarning(false);
+  };
 
   // const _ = useSelector((state) => {
   //   return state.surveyData.messages;
   // });
 
   const showReplyModal = () => {
-    updateState()
+    updateState();
     setVisible(true);
-    console.log({modalTextContent}, props.msg.content); 
-  };
-  const showEditModal = () => {
-    updateState()
-    setModalTextContent(props.msg.content);
-    setModalTextLabel(props.msg.label);
-    setModelToMessageId(props.msg._id);
-    setVisibleEdit(true);
+    console.log({ modalTextContent }, props.msg.content);
   };
 
   const handleOk = () => {
-    console.log({modalTextContent}, modalTextContent === ""); 
+    console.log({ modalTextContent }, modalTextContent === "");
     // setModalText("The modal will be closed after two seconds");
     if (modalTextContent === "") {
       setDisplayReplyWarning(true);
@@ -104,25 +97,12 @@ export function MessageOptionMenu(props) {
         modelToMessageId
       );
 
-      updateState()
-    }
-  };
-  const handleEditOk = (event) => {
-    console.log({modalTextContent}, modalTextContent === ""); 
-    // setModalText("The modal will be closed after two seconds");
-    if (modalTextContent === "") {
-      setDisplayReplyWarning(true);
-    } else {
-      setDisplayReplyWarning(false);
-      console.log(displayReplyWarning);
-      setConfirmLoading(true);
-      sendMessageToBackend(props.msg._id, modalTextContent, modalTextLabel);
-      updateState()
+      updateState();
     }
   };
 
   const handleCancel = () => {
-    updateState()
+    updateState();
   };
 
   const handleNewOptionTextContent = (event) => {
@@ -135,6 +115,19 @@ export function MessageOptionMenu(props) {
     setModelToMessageId(event);
     // setModalTextLabel(event.target.value);
   };
+
+  function onBlur() {
+    console.log('blur');
+  }
+  
+  function onFocus() {
+    console.log('focus');
+  }
+  
+  function onSearch(val) {
+    console.log('search:', val);
+  }
+
   return (
     <div>
       <Space size="middle">
@@ -149,28 +142,6 @@ export function MessageOptionMenu(props) {
         confirmLoading={confirmLoading}
         onCancel={handleCancel}
       >
-        <Input placeholder="Content" onChange={handleNewOptionTextContent} 
-          value={modalTextContent}/>
-        <BotWarning displayReplyWarning={displayReplyWarning}></BotWarning>
-        <Input
-          placeholder="Label (Optional)"
-          onChange={handleNewOptionTextLabel}
-          value={modalTextLabel}
-        />
-        <Select style={{ width: 120 }} onChange={onMessageChange} value={modelToMessageId}>
-          {props.msgList.map((msg) => {
-            if (msg._id !== props.msg._id)
-              return <Option key={msg._id}>{msg.content}</Option>;
-          })}
-        </Select>
-      </Modal>
-      <Modal
-        title="Edit Message"
-        visible={visibleEdit}
-        onOk={handleEditOk}
-        confirmLoading={confirmLoading}
-        onCancel={handleCancel}
-      >
         <Input
           placeholder="Content"
           onChange={handleNewOptionTextContent}
@@ -182,6 +153,24 @@ export function MessageOptionMenu(props) {
           onChange={handleNewOptionTextLabel}
           value={modalTextLabel}
         />
+        <Select
+          showSearch
+          style={{ width: 472 }}
+          placeholder="Select to message"
+          optionFilterProp="children"
+          onChange={onMessageChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          {props.msgList.map((msg) => {
+            if (msg._id !== props.msg._id)
+              return <Option key={msg._id}>{msg.content}</Option>;
+          })}
+        </Select>
       </Modal>
     </div>
   );
