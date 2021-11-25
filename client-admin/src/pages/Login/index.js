7280f621d182844539ffec7f61db96ac9a11a1b7
themarkyone <markyin2001@gmail.com>
2021-11-24 22:55:24 -0500

    update on statistics graphs

--- client-admin/src/pages/Login/index.js
+++ client-admin/src/pages/Login/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./index.scss"
+import "./index.css"
 import { useHistory } from 'react-router-dom';
 
 import { Form, Input, Button, message } from 'antd';
--- client-admin/src/pages/Statistics/index.js
+++ client-admin/src/pages/Statistics/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import StageDemoPie from 'components/Statistics/StageDemoPie';
+import VisitByLocationLines from 'components/Statistics/VisitByLocationLines'
+import VisitByLocationChart from 'components/Statistics/VisitByLocationChart'
+import VisitByQuestionChart from 'components/Statistics/VisitByQuestionChart'
 import Menu from "components/Menu";
 import { Layout } from 'antd';
+import './index.css'
 const { Header, Content } = Layout;
 
 
@@ -15,7 +20,22 @@ class StatisticsPage extends React.Component {
                     <Header style={{ "backgroundColor": "white" }}><h1>NM Bot Statistics</h1></Header>
 
                     <Content style={{ padding: '1rem' }}>
-                        
+                        <div className="summary-section">
+                            <div>The overview of people from different area</div>
+                            <VisitByLocationLines />
+                        </div>
+                        <div className="summary-section">
+                            <div>The percentage of people from different area in Egypt</div>
+                            <VisitByLocationChart />
+                        </div>
+                        <div className="summary-section">
+                            <div>The percentage of people from different questions in our website</div>
+                            <VisitByQuestionChart />
+                        </div>
+                        <div className="summary-section">
+                            <div>The percentage of different stage</div>
+                            <StageDemoPie />
+                        </div>
                     </Content>
 
                 </Layout>
