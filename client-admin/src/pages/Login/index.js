5a71c59eb8dbd67b5d5c37c5c69891abda9f51b5
Yuxuan Liu <yuxuanleon.liu@mail.utoronto.ca>
2021-12-01 16:40:41 -0500

    Updated Login style

--- client-admin/src/pages/Login/index.js
+++ client-admin/src/pages/Login/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import "./index.css"
+// import "./index.css"
 import { useHistory } from 'react-router-dom';
 
-import { Form, Input, Button, message } from 'antd';
+import { Form, Input, Button, message, Row, Layout, Col } from 'antd';
 
 import { loginAdmin, logoutAdmin } from 'actions/Auth'
 
@@ -10,13 +10,12 @@ function LoginPage() {
     const history = useHistory();   // Used for redirection
     // Coming to the login page, we first logout the user.
     logoutAdmin()
-    
+
     /**
      * When user click LOGIN, send request to verify input. If valid, redirect to home page.
      * @param {*} values {username, password}
      */
     const onFinish = async (values) => {
-        // console.log('Received values of form: ', values);
         try {
             const loginSuccess = await loginAdmin(values)
             if (loginSuccess) {
@@ -25,47 +24,53 @@ function LoginPage() {
             } else {
                 message.warning("Invalid Credentials!")
             }
-        } catch(error) {
+        } catch (error) {
             message.error(String(error))
         }
     };
 
     return (
-        <div className="login-page">
-            <div className="login-container">
-                <h1>Welcome to NM Bot Admin Dashboard</h1>
+        <Layout className="login-page" style={{ height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignContent: "center" }}>
+            <Col className="login-container" span={10} offset={7} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
+                <Row>
+                    <h1>Welcome to NM Bot Admin Dashboard</h1>
+                </Row>
 
-                <Form
-                    name="basic"
-                    labelCol={{ span: 6 }}
-                    wrapperCol={{ span: 14 }}
-                    onFinish={onFinish}
-                    autoComplete="off"
-                >
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
-                    >
-                        <Input />
-                    </Form.Item>
+                <Row style={{ width: "80%", marginTop: "2rem" }}>
+                    <Col span={24}>
+                        <Form
+                            name="basic"
+                            labelCol={{ span: 5, offset: 2 }}
+                            wrapperCol={{ span: 12, offset: 1 }}
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[{ required: true, message: 'Please input your username!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
-                        <Input.Password />
-                    </Form.Item>
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your password!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
 
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" htmlType="submit">
-                            Login
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </div>
-        </div>
+                            <Form.Item wrapperCol={{ offset: 10, span: 4 }}>
+                                <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+                                    Login
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                </Row>
+            </Col>
+        </Layout>
     )
 }
 
